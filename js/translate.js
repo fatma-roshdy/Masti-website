3758e02fd95ac917ad98fa0e348e338db3fb2658
fatma-roshdy <fatmaROSHDY@outlook.com>
2019-06-10 20:19:58 +0200

    upload app

---
+++ js/translate.js
@@ -0,0 +1,183 @@
+let arrlang={
+    "en":{
+        "date":"Mon - Sat: 7:00 - 17:00",
+        "number":"+ 386 40 111 5555",
+        "email":"info@yourdomain.com",
+        "home":"HOME",
+        "about":"ABOUT",
+        "pages":"PAGES",
+        "projects":"PROJECTS",
+        "blog":"BLOG",
+        "contact":"CONTACT US",
+        "english":"English",
+        "arabic":"Arabic",
+        "title-header":"We care about your business ",
+        "contant-about":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam elementum consectetur egestas. Nulla nisi nisi, rutrum vitae nunc non, bibendum euismod nulla ",
+        "btn-about":"ABOUT US",
+        "num-client1":"456",
+        "client1":"HAPPY CLIENTS",
+        "num-client2":"56",
+        "client2":"PROJECTS",
+        "num-client3":"456",
+        "client3":"HAPPY CLIENTS",
+        "num-client4":"456",
+        "client4":"HAPPY CLIENTS",
+        "blog-h2":"FROM BLOG",
+        "date1":"23 oct 2015",
+        "content1":"Dummy text is text that is used in theublish industry or by web designers to occupy the space which will later be ",
+        "span-data1":"read more",
+        "get-touch":"GO AHEAD AND FIND OUT HOW WE CAN HELP YOU!",
+        "btn-get-touch":"GET IN TOUCH",
+        "blog-news":"BLOG",
+        "card-title-news":"Steps Toward Developing A Content Strategy",
+        "card-content-news":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab debitis ea est illum ipsa itaque liberop tiovolup Conseq uuntur deserunt expedita, fugiat hic illum porro quidem quis recusandae vero? Aliquiassumenda cum delectus eaque eligendi, enim eum excepturi fugit illum impedit in iste laudantium modi natus, nisi nobis gfy  g obcaecati praesentium quis reiciendis rerum sapiente […]",
+        "button-card-news":"READ MORE",
+        "date":"march 24,2015",
+        "date2":"febbbbb 28,2015",
+        "copyright":" by Revolthemes.net All Rights Reserved",
+        "tit-footer1":"About Us",
+        "text-footer111":"Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen",
+        "title-footer2":"Related Links",
+        "text1":"Official Site",
+        "text2":"CreativeMarket profile",
+        "text3":"Materialzer page on CM",
+        "text4":"Electronic WP Theme",
+        "text5":"was favvvvv",
+        "title-footer3":"News & Media ",
+        "text-footer1":" Events",
+        "text-footer2":" News and Press Releases",
+        "text-footer3":"Interviews",
+        "text-footer4":" Subsidiaries Media Contacts",
+        "text-footer5":"Media Kit",
+        "title-footer3":"GET IN TOUCH",
+        "text-footer33":"	Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        "text-blog-slider":"Dummy text is text that is used in theublish industry or by web designers to occupy the space which will later be",
+        "date-blog-slider":"23 oct 2015",
+        "title-show1":"Nibh Ridiculus",
+        "title-show2":"grammatica del",
+        "title-show3":"Europan lingues",
+        "title-show4":"ipsum quia",
+        "title-show5":"aliquam quaerat",
+        "title-show6":"dolorem eum",
+        "text-show1":"Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus etmagnis dis parturie nt montes, nascetur.",
+        "text-show2":"Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus etmagnis dis parturie nt montes, nascetur.",
+        "text-show3":"Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus etmagnis dis parturie nt montes, nascetur.",
+        "text-show4":"Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus etmagnis dis parturie nt montes, nascetur.",
+        "text-show5":"Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus etmagnis dis parturie nt montes, nascetur.",
+        "text-show6":"Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus etmagnis dis parturie nt montes, nascetur.",
+        
+    },
+    "ar":{
+        "date":"الاثنين-السبت:7:00-17:00",
+        "number":"+ 386 40 111 5555",
+        "email":"info@yourdomain.com",
+        "home":"الرئسية",
+        "about":"ماذا عن",
+        "pages":"الصفحات",
+        "projects":"المشاريع",
+        "blog":"مدونة",
+        "contact":"اتصل بنا",
+        "english":"الانجليزيه",
+        "arabic":"العربية",
+        "title-header":"نحن حريصون علي مشروعك",
+        "contant-about":"نحن نوفر لك كل السبل من اجل ان تتم مشروعك بنجاح وتوفيق ",
+        "btn-about":"معومات تهمك",
+        "num-client1":"٤٥٦",
+        "client1":"الزبائن السعداء",
+        "num-client2":"٥٦",
+        "client2":"المشاريع",
+        "num-client3":"٤٥٦",
+        "client3":"الزبائن السعداء",
+        "num-client4":"٤٥٦",
+        "client4":"الزبائن السعداء",
+        "blog-h2":"من البلوج",
+        "date1":"٢٣أكتوبر ٢٠١٥",
+        "content1":"النص الوهمي هو نص يُستخدم في الصناعة المنشورة أو من قِبل مصممي الويب لشغل المساحة التي ستكون لاحقًا ",
+        "span-data1":"اقرا المزيد",
+        "get-touch":"انطلق واكتشف كيف يمكننا مساعدتك!",
+        "btn-get-touch":"الحصول على اتصال",
+        "blog-news":"المدونة",
+        "card-title-news":"خطوات نحو تطوير استراتيجية محتوى",
+        "card-content-news":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab debitis ea est illum ipsa itaque liberop tiovolup Conseq uuntur deserunt expedita, fugiat hic illum porro quidem quis recusandae vero? Aliquiassumenda cum delectus eaque eligendi, enim eum excepturi fugit illum impedit in iste laudantium modi natus, nisi nobis gfy  g obcaecati praesentium quis reiciendis rerum sapiente […]",
+        "button-card-news":"اقرا المزيد",
+        "date":"march 24,2015",
+        "date2":"مارس,23,2019",
+        "copyright":" Revolthemes جميع الحقوق محفوظة بواسطه ",
+        "tit-footer1":"ماذا عنا",
+        "text-footer111":"أنتم أوروبيون أعضاء في عائلة سام. إنهم منفصلون ، هناك خرافة. بالنسبة للعلم والموسيقى والرياضة وغيرها ، فإن أوروبا تستخدم مفرداتها. تتحدث بشكل مختلف.",
+        "title-footer2":"اللينكات المتعلقه",
+        "text1":"الموقع الرسمى",
+        "text2":"ملف شخصي",
+        "text3":"صفحه مازليزا على سن",
+        "text4":"اليكرونيات ",
+        "text5":"بواسطه فاف",
+        "title-footer3":"الأخبار والإعلام ",
+        "text-footer1":" احداث",
+        "text-footer2":" الأخبار والبيانات الصحفية",
+        "text-footer3":"المقابلات",
+        "text-footer4":" جهات الاتصال الإعلامية",
+        "text-footer5":"مجموعة الوسائط",
+        "title-footer3":"الحصول على اتصال",
+        "text-footer33":"أبجد هوز هو مجرد دمية النص لصناعة الطباعة والتنضيد. ",
+        "text-blog-slider":"النص الوهمي هو نص يُستخدم في الصناعة أو بواسطة مصممي الويب لشغل المساحة التي ستكون في وقت لاحق",
+        "date-blog-slider":"23 اكتوبر 2015",
+        "title-show1":"مصممى الويب",
+        "title-show2":"مصممى الويب",
+        "title-show3":"مصممى الويب",
+        "title-show4":"مصممى الويب",
+        "title-show5":"مصممى الويب",
+        "title-show6":"مصممى الويب",
+        "text-show1":"النص الوهمى هو نص يستخدم في الصناعه او بواسطه مصممى الويب لشغل المساحه التى ستكون في وقت ما",
+        "text-show2":"النص الوهمى هو نص يستخدم في الصناعه او بواسطه مصممى الويب لشغل المساحه التى ستكون في وقت ما",
+        "text-show3":"النص الوهمى هو نص يستخدم في الصناعه او بواسطه مصممى الويب لشغل المساحه التى ستكون في وقت ما",
+        "text-show4":"النص الوهمى هو نص يستخدم في الصناعه او بواسطه مصممى الويب لشغل المساحه التى ستكون في وقت ما",
+        "text-show5":"النص الوهمى هو نص يستخدم في الصناعه او بواسطه مصممى الويب لشغل المساحه التى ستكون في وقت ما",
+        "text-show6":"النص الوهمى هو نص يستخدم في الصناعه او بواسطه مصممى الويب لشغل المساحه التى ستكون في وقت ما",
+       
+        
+    }
+
+
+};
+
+$(function(){
+        let x=localStorage.getItem("langu");
+        if(x=="ar"){         
+            $(".lang").each(function(index,element){
+            $(this).text(arrlang[x][$(this).attr('key')]);           
+            })}
+        else if(x=="en"){
+          
+            $(".lang").each(function(index,element){
+            $(this).text(arrlang[x][$(this).attr('key')]);
+           
+            
+        })}
+    $('.translate').click(function(){
+        var lang=$(this).attr("id");
+        localStorage.setItem("langu",lang);
+        $(".lang").each(function(index,element){
+            $(this).text(arrlang[lang][$(this).attr('key')]);
+        })
+    }
+)});
+
+$(function() { 
+    
+ 
+    $("#ar").click(function(){
+        
+        $("html").attr("dir", function(){
+            $("html").attr("dir", "rtl");
+            $("body").css("text-align","right");
+        });
+    });
+
+    $("#en").click(function(){
+        
+        $("html").attr("dir", function(){
+            $("html").attr("dir", "ltr");
+            $("body").css("text-align","left");
+        });
+    });
+});
